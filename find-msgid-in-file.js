1d9b3fc9c0dab14444f80e773ce0b82e6ad3f520
Mithgol the Webmaster <getgit@mithgol.ru>
2016-12-02 00:35:25 +0300

    v0.1.0: initial working version (minimum viable product) unless buggy

--- find-msgid-in-file.js
+++ find-msgid-in-file.js
@@ -1,1 +1,19 @@
-module.exports = 'Hello world.';
+var fs = require('fs');
+
+module.exports = (filePath, callback) => fs.readFile(
+   filePath,
+   { encoding: 'binary' },
+   (err, message) => {
+      if( err ) return callback(err);
+
+      var lineMSGID = message.split(
+         /(?:\r|\n)/
+      ).find( msgLine => msgLine.startsWith('\x01MSGID:') );
+      if( typeof lineMSGID === 'undefined') return callback(null, null);
+
+      lineMSGID = lineMSGID.slice( '\x01MSGID:'.length ).replace(/^\s*/g, '');
+      if( lineMSGID.length < 1 ) return callback(null, null);
+
+      return callback(null, lineMSGID);
+   }
+);
